fca06ee9fff59c899f5855f5f83a124a59f6fa23
Amer Harb <amer.harb@gmail.com>
2024-04-23 01:01:46 +0200

    next.4

--- packages/numerals-client/src/index.ts
+++ packages/numerals-client/src/index.ts
@@ -1,15 +1,19 @@
-import { convert } from '@numerals/eastern-arabic'
+// import { convert } from '@numerals/eastern-arabic'
 
 log('@numerals/eastern-arabic')
 
 log(calmConvert(123))
 log(calmConvert(123.456))
+log(calmConvert(-1))
+log(calmConvert(NaN))
+
 
 log('Done')
 
 function calmConvert(source: number): string {
 	try {
-		return convert(source)
+		// return convert(source)
+		return 'disable temporarily'
 	} catch (e: any) {
 		return e.message
 	}
