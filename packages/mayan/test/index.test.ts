a34134e1f5d4b50023701f8f608dcbf180f0cb73
Amer Harb <amer.harb@gmail.com>
2024-04-26 22:33:02 +0200

    add unit test

--- packages/mayan/test/index.test.ts
+++ packages/mayan/test/index.test.ts
@@ -1,22 +1,39 @@
 import { convert } from '../src'
+import { Ma } from '../src/numerals'
 import { describe, expect, it } from '@jest/globals'
 
 describe('convert()', () => {
 	it('convert 0 into 𝋠', () => {
 		const actual = convert(0)
-		expect(actual).toEqual('𝋠')
+		expect(actual).toEqual(Ma['0'])
 	})
 	it('convert 1 into 𝋡', () => {
 		const actual = convert(1)
-		expect(actual).toEqual('𝋡')
+		expect(actual).toEqual(Ma['1'])
 	})
 	it('convert 10 into 𝋪', () => {
 		const actual = convert(10)
-		expect(actual).toEqual('𝋪')
+		expect(actual).toEqual(Ma['10'])
 	})
 	it('convert 19 into 𝋳', () => {
 		const actual = convert(19)
-		expect(actual).toEqual('𝋳')
+		expect(actual).toEqual(Ma['19'])
+	})
+	it('convert 20 into 𝋰𝋠', () => {
+		const actual = convert(20)
+		expect(actual).toEqual(`${Ma['1']}${Ma['0']}`)
+	})
+	it('convert 21 into 𝋡𝋡', () => {
+		const actual = convert(21)
+		expect(actual).toEqual(`${Ma['1']}${Ma['1']}`)
+	})
+	it('convert 39 into 𝋡𝋳', () => {
+		const actual = convert(39)
+		expect(actual).toEqual(`${Ma['1']}${Ma['19']}`)
+	})
+	it('convert 40 into 𝋢𝋠', () => {
+		const actual = convert(40)
+		expect(actual).toEqual(`${Ma['2']}${Ma['0']}`)
 	})
 	it.each([NaN, Infinity, -Infinity])
 	('throw Error for non number [%s]', (it) => {
